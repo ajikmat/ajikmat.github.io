50f0089e03038f42bd6a027f35bf2766c6e9397b
Haziq Mohamad <haziqsk939@gmail.com>
2023-12-23 18:35:25 +0800

    feat: responsive design

--- src/config/career.js
+++ src/config/career.js
@@ -1,14 +1,10 @@
 export default [
     {
         timeline: 'March 2023 - Present',
-        company: 'TARIKEMAS DOT COM SDN. BHD.',
+        company: 'Tarikemas Dot Com Sdn. Bhd.',
         title: 'Frontend Developer',
-        description: [
-            'Developed robust and scalable mobile applications using the BLoC (Business Logic Component) pattern to effectively manage data flow and business logic.',
-            'Collaborate with business analyst teams to address reported issues and perform debugging to identify and resolve any application defects.',
-            "Ensuring the smooth operation and continuous improvement of the client's existing application",
-            'Worked closely with backend developers to test and troubleshoot API integration, ensuring proper handling of data, error responses, and edge cases.',
-        ],
+        description:
+            "Assigned to two projects—an e-commerce application and a payment service application. Responsible for enhancing features in an existing client's e-commerce app and leading the setup and development of the front-end architecture for the payment service application.",
         stack: [
             'svg/flutter.svg',
             'svg/dart.svg',
@@ -19,25 +15,18 @@ export default [
     },
     {
         timeline: 'November 2022 - March 2023',
-        company: 'AWFATECH GLOBAL SDN. BHD.',
+        company: 'Awfatech Global Sdn. Bhd.',
         title: 'Flutter Developer',
-        description: [
-            'Design and develop and mobile applications using MVC architecture.',
-            'Migrate an Angular web application into the Flutter framework.',
-            'Integrate APIs and collaborate with backend developers to ensure full functionality and integration of applications.',
-            'Proficiently maintained and debugged existing Flutter applications, ensuring smooth operation and optimal performance.',
-        ],
+        description:
+            'My first company after graduate. Proficiently maintained and debugged existing Flutter applications, ensuring smooth operation and optimal performance.',
         stack: ['svg/flutter.svg', 'svg/dart.svg'],
     },
     {
         timeline: 'July 2021 - October 2021',
-        company: 'Centre for Information Technology Advancement, IIUM',
+        company: 'International Islamic University Malaysia',
         title: 'Intern - Flutter Developer',
-        description: [
-            'Designed and developed an progressive web application called IIUM Tech Support which streamlined task assignment from staff to technicians.',
-            'Integrated Cloud Firestore database with Flutter framework.',
-            'Convert the design into actual coding.',
-        ],
+        description:
+            'Designed and developed an progressive web application using Flutter called IIUM Tech Support which streamlined task assignment from staff to technicians.',
         stack: ['svg/flutter.svg', 'svg/dart.svg'],
     },
 ];
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,28 @@
 <template>
     <div class="flex flex-col">
-        <h3 class="text-5xl font-bold text-primary-900 mb-8">
-            Hello, I'm <span class="text-primary-600">Haziq Mohamad</span>!
+        <h3 class="text-3xl md:text-5xl font-bold text-primary-900 mb-8">
+            Hello, I'm
+            <span
+                class="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#36d1dc] to-[#5b86e5]"
+                >Haziq Mohamad</span
+            >!
         </h3>
 
-        <div class="flex gap-8 mb-12">
+        <div class="flex flex-col-reverse sm:flex-row gap-8 mb-12">
             <div class="flex flex-col gap-8 items-start">
                 <div class="border-l-4 border-slate-500 ps-4 flex">
-                    <p class="text-base text-slate-600">
+                    <p class="text-sm sm:text-base text-slate-600">
                         Experienced
                         <span class="font-semibold text-slate-800">Frontend Developer</span> with
                         over a year's expertise, specializing in
-                        <span class="font-semibold text-green-600">VueJS</span> and
+                        <span class="font-semibold text-emerald-600">VueJS</span> and
                         <span class="font-semibold text-primary-600">Flutter</span>. Proficient in
                         crafting powerful and intuitive user interfaces to elevate the digital
                         experience.
                     </p>
                 </div>
 
-                <div class="flex flex-col gap-1 ps-5 -mt-2 w-full">
+                <div class="md:flex flex-col gap-1 ps-5 -mt-2 w-full hidden">
                     <div class="flex gap-1">
                         <MapPinIcon class="w-5 text-slate-500" />
                         <p class="font-medium text-sm text-slate-700">Ampang, Kuala Lumpur</p>
@@ -38,14 +42,14 @@
                 <a
                     href="https://drive.google.com/file/d/1sblovEpHJUPW7nz-Hl3MXhscIspncVw-/view?usp=sharing"
                     target="_blank"
-                    class="rounded-lg px-4 py-2 border shadow-sm font-semibold text-white hover:border-slate-300 bg-primary-500 hover:bg-primary-600"
+                    class="mx-auto sm:mx-0 rounded-lg px-4 py-2 border shadow-sm font-semibold text-white hover:border-slate-300 bg-primary-500 hover:bg-primary-600"
                 >
                     My Resume
                 </a>
             </div>
 
             <div
-                class="h-64 flex-shrink-0 border w-64 rounded-lg overflow-clip shadow-lg bg-slate-100"
+                class="size-48 md:size-64 flex-shrink-0 border rounded-full mx-auto overflow-clip shadow-lg bg-slate-100"
             >
                 <img
                     src="/images/profile_picture.jpeg"
@@ -58,19 +62,19 @@
         <section class="mb-12">
             <h2 class="text-2xl font-semibold mb-4">My Stack</h2>
 
-            <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-                <div
-                    v-for="stack in stacks"
-                    :key="stack.title"
-                    class="group flex p-4 border rounded gap-2 items-center shadow-sm hover:bg-slate-50 cursor-pointer hover:-translate-y-1 transition-all"
-                >
-                    <img
-                        :src="getAssetPath(stack.svgSrc)"
-                        class="w-5 h-5 grayscale group-hover:grayscale-0"
-                        :alt="stack.title"
-                    />
+            <div class="grid grid-cols-12 gap-4">
+                <div class="col-span-6 md:col-span-4" v-for="stack in stacks" :key="stack.title">
+                    <div
+                        class="group flex p-4 border rounded gap-2 items-center shadow-sm hover:bg-slate-50 cursor-pointer hover:-translate-y-1 transition-all"
+                    >
+                        <img
+                            :src="getAssetPath(stack.svgSrc)"
+                            class="w-5 h-5 md:grayscale group-hover:grayscale-0"
+                            :alt="stack.title"
+                        />
 
-                    <p class="font-semibold flex-none">{{ stack.title }}</p>
+                        <p class="font-semibold truncate">{{ stack.title }}</p>
+                    </div>
                 </div>
             </div>
         </section>
@@ -122,31 +126,28 @@
             <h2 class="text-2xl font-semibold mb-4">Career</h2>
 
             <ul class="border-l border-slate-300 ms-3 py-4">
-                <li class="relative ms-10 mb-4" v-for="(career, index) in careers" :key="index">
+                <li
+                    class="relative ms-4 sm:ms-10 mb-4"
+                    v-for="(career, index) in careers"
+                    :key="index"
+                >
                     <div
-                        class="absolute top-4 -left-[46.5px] w-3 h-3 rounded-full bg-slate-700 ring-4 ring-white"
+                        class="absolute top-4 -left-[22.5px] sm:-left-[46.5px] w-3 h-3 rounded-full bg-slate-700 ring-4 ring-white"
                     />
                     <div class="border rounded py-4 px-8">
-                        <h6 class="font-medium text-slate-500 mb-2">
+                        <h6 class="text-sm sm:text-base font-medium text-slate-500 mb-2">
                             {{ career.timeline }}
                         </h6>
-                        <h4 class="text-xl font-semibold text-slate-800">{{ career.title }}</h4>
+                        <h4 class="text-base sm:text-xl font-semibold text-slate-800">
+                            {{ career.title }}
+                        </h4>
 
-                        <h4 class="text-base font-semibold text-slate-600 mb-2">
+                        <h4 class="text-sm sm:text-base font-semibold text-slate-600 mb-2">
                             {{ career.company }}
                         </h4>
 
                         <div class="text-sm text-slate-500 mb-4">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio inventore
-                            molestias dolorem nobis minus, repellat ab alias numquam voluptas ipsa
-                            maiores, eligendi sunt saepe cupiditate itaque hic similique distinctio
-                            mollitia.
-                        </div>
-
-                        <div class="md:flex gap-2 justify-start hidden">
-                            <template v-for="stack in career.stack">
-                                <img class="w-6 h-6 grayscale hover:grayscale-0" :src="stack" />
-                            </template>
+                            {{ career.description }}
                         </div>
                     </div>
                 </li>
